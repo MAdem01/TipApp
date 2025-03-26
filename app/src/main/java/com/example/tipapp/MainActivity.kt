14de9c2cdfc8b30bcc83045520b4b579219848e0
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-03-26 18:04:01 +0100

    add TopHeader composable layout

--- app/src/main/java/com/example/tipapp/MainActivity.kt
+++ app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tipapp.ui.theme.TipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +29,20 @@ class MainActivity : ComponentActivity() {
             MyApp {
                 Text(text = "Hello again")
             }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopHeader() {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
+    ) {
+        Column() {
+
         }
     }
 }
