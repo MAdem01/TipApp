fdd443568faf7743bcf185f2aecf568d04e0b437
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-03-26 18:20:33 +0100

    add TopHeader Text composables

--- app/src/main/java/com/example/tipapp/MainActivity.kt
+++ app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -3,21 +3,22 @@ package com.example.tipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tipapp.ui.theme.TipAppTheme
@@ -27,7 +28,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "Hello again")
+                TopHeader()
             }
         }
     }
@@ -35,14 +36,27 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 134.0) {
     Surface(modifier = Modifier
         .fillMaxWidth()
         .height(150.dp)
-        .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
+        .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
     ) {
-        Column() {
-
+        Column(modifier = Modifier
+            .padding(12.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.ExtraBold)
         }
     }
 }
@@ -60,6 +74,6 @@ fun MyApp(content: @Composable () -> Unit){
 @Composable
 fun GreetingPreview() {
         MyApp{
-            Text(text = "Hello Again")
+
         }
 }
